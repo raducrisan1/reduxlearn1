bd7c3abab7bcd16cf7993cfcb0dd14fe19773804
Radu Crisan <radu.crisan@gmail.com>
2016-10-24 00:18:29 +0300

    error handling

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,5 +1,7 @@
 import * as types from './actionTypes';
 import authorsApi from '../api/mockAuthorApi';
+import {beginAjaxCall} from './ajaxStatusActions';
+
 
 export function loadAuthorsSuccess(authors) {
   return {type: types.LOAD_AUTHORS_SUCCESS, authors};
@@ -7,6 +9,7 @@ export function loadAuthorsSuccess(authors) {
 
 export function loadAuthors() {
   return function(dispatch) {
+    dispatch(beginAjaxCall());
     return authorsApi.getAllAuthors().then(authors => {
       dispatch(loadAuthorsSuccess(authors));
     }).catch(error => {
--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -3,6 +3,7 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseForm from './CourseForm';
+import toastr from 'toastr';
 
 class ManageCoursePage extends React.Component {
   constructor(props, context) {
@@ -10,7 +11,8 @@ class ManageCoursePage extends React.Component {
 
     this.state = {
       course: Object.assign({}, props.course),
-      errors: {}
+      errors: {},
+      saving: false
     };
 
     this.updateCourseState = this.updateCourseState.bind(this);
@@ -32,7 +34,19 @@ class ManageCoursePage extends React.Component {
 
   saveCourse(event) {
     event.preventDefault();
-    this.props.actions.saveCourse(this.state.course);
+    this.setState({saving: true});
+    this.props.actions.saveCourse(this.state.course)
+    .then(() => this.redirect())
+      .catch(error => {
+        this.setState({saving: false});
+        toastr.error(error);
+      });
+
+  }
+
+  redirect() {
+    this.setState({saving: false});
+    toastr.success('Course saved');
     this.context.router.push('/courses');
   }
 
@@ -42,6 +56,7 @@ class ManageCoursePage extends React.Component {
         allAuthors={this.props.authors}
         course={this.state.course}
         errors={this.state.errors}
+        saving={this.state.saving}
         onChange={this.updateCourseState}
         onSave={this.saveCourse}
       />
